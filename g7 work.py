82dbd085f71021fc5bb2bd423afcd263eb1c3bdb
33609 <112545682+33609@users.noreply.github.com>
2023-01-27 15:36:54 +0700

    Add files via upload

---
+++ g7 work.py
@@ -0,0 +1,11 @@
+number1 = int(input('enter a number '))
+number2 = int(input('enter a another number '))
+ans = input("Do you want to add Y/N ")
+if  ans == "Y":
+    result = number1+number2
+    
+else:
+    result = number1-number2
+    print(result)
+
+    
